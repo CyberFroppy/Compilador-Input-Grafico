b3137bd8e8d0d62ccefa1d96d03dae1030244149
[CyberFroppy] <CyberFroppy@users.noreply.github.com>
2020-11-23 20:48:30 -0600

    Se agrego maquina virtual

---
+++ MaquinaVirtual/maquina.py
@@ -0,0 +1,53 @@
+import sys
+
+def error(message):
+  print(message)
+
+program_name = sys.argv[1]
+with open(program_name, 'r') as file:
+    global symbol_table, cuadruplos, raw_constant_table
+    input = eval(file.read())
+    symbol_table = input['symbol_table']
+    cuadruplos = input['cuadruplos']
+    raw_constant_table = input['constant_table']
+
+
+global mem_global, mem_local, constant_table
+mem_global = {}
+mem_local = []
+constant_table = {}
+for key, val in raw_constant_table.items():
+    constant_table[val['address']] = key
+
+global current 
+
+def read(address, depth=-1):
+    val = mem_local[depth].get(address, None)
+    if not val:
+        val = mem_global.get(address, None)
+        if not val:
+            val = constant_table.get(address, None)
+            if not val:
+                error('Acceso a variable no asignada')
+                current = -1
+                return
+    return val
+
+
+def write(address, value, depth=-1):
+    global mem_local, mem_global
+    if address >= 3000 and address < 6000:
+        mem_local[depth][address] = value
+    else: 
+        mem_global[address] = value
+
+
+current = [0]
+while current[-1] != -1:
+    if cuadruplos[current[-1]][0] == 'START':
+        current[-1] = cuadruplos[current[-1]][3]
+    elif cuadruplos[current[-1]][0] == 'END':
+        current[-1] = -1
+    else:
+        print('Instruccion no implementada: ', cuadruplos[current[-1]][0])
+        current[-1] = -1
