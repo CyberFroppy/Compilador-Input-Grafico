5e19ed383217389a3e7ed75ea32869e7e6f36d51
[CyberFroppy] <CyberFroppy@users.noreply.github.com>
2020-11-24 17:49:25 -0600

    Maquina virtual -Marlon-

--- MaquinaVirtual/maquina.py
+++ MaquinaVirtual/maquina.py
@@ -22,24 +22,24 @@ for key, val in raw_constant_table.items():
 global current 
 
 def read(address, depth=-1):
-    val = mem_local[depth].get(address, None)
+    # val = mem_local[depth].get(address, None)
+    # if not val:
+    val = mem_global.get(address, None)
     if not val:
-        val = mem_global.get(address, None)
+        val = constant_table.get(address, None)
         if not val:
-            val = constant_table.get(address, None)
-            if not val:
-                error('Acceso a variable no asignada')
-                current = -1
-                return
+            error('Acceso a variable no asignada')
+            current = -1
+            return
     return val
 
 
 def write(address, value, depth=-1):
     global mem_local, mem_global
-    if address >= 3000 and address < 6000:
-        mem_local[depth][address] = value
-    else: 
-        mem_global[address] = value
+    # if address >= 3000 and address < 6000:
+    #     mem_local[depth][address] = value
+    # else: 
+    mem_global[address] = value
 
 
 current = [0]
@@ -48,6 +48,51 @@ while current[-1] != -1:
         current[-1] = cuadruplos[current[-1]][3]
     elif cuadruplos[current[-1]][0] == 'END':
         current[-1] = -1
+    elif cuadruplos[current[-1]][0] == '+':
+        #print('hola')
+        valor1 = int(read(cuadruplos[current[-1]][1]))
+        #print(valor1)
+        valor2 = int(read(cuadruplos[current[-1]][2]))
+        #print(valor2)
+        res = valor1 + valor2
+        #print(res)
+        write(cuadruplos[current[-1]][3], res)
+        #print(cuadruplos[current[-1]][3])
+        current[-1] = current[-1]+1
+    elif cuadruplos[current[-1]][0] == '-':
+        #print('hola')
+        valor1 = int(read(cuadruplos[current[-1]][1]))
+        #print(valor1)
+        valor2 = int(read(cuadruplos[current[-1]][2]))
+        #print(valor2)
+        res = valor1 - valor2
+        #print(res)
+        write(cuadruplos[current[-1]][3], res)
+        #print(cuadruplos[current[-1]][3])
+        current[-1] = current[-1]+1
+    elif cuadruplos[current[-1]][0] == '*':
+        valor1 = int(read(cuadruplos[current[-1]][1]))
+        valor2 = int(read(cuadruplos[current[-1]][2]))
+        res = valor1 * valor2
+        write(cuadruplos[current[-1]][3], res)
+        #print(cuadruplos[current[-1]][3])
+        current[-1] = current[-1]+1
+    elif cuadruplos[current[-1]][0] == '/':
+        valor1 = int(read(cuadruplos[current[-1]][1]))
+        valor2 = int(read(cuadruplos[current[-1]][2]))
+        res = valor1 / valor2
+        write(cuadruplos[current[-1]][3], res)
+        #print(cuadruplos[current[-1]][3])
+        current[-1] = current[-1]+1
+    elif cuadruplos[current[-1]][0] == '=':
+        valor1 = int(read(cuadruplos[current[-1]][1]))
+        write(cuadruplos[current[-1]][3], valor1)
+        #print(cuadruplos[current[-1]][3])
+        current[-1] = current[-1]+1
+    elif cuadruplos[current[-1]][0] == 'PRINT':
+        valor1 = int(read(cuadruplos[current[-1]][3]))
+        print(valor1)
+        current[-1] = current[-1]+1
     else:
         print('Instruccion no implementada: ', cuadruplos[current[-1]][0])
         current[-1] = -1
